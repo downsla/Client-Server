f37f6c118e22deddabe1455719913fafdc4149e2
Liam Downs <downs.liama@gmail.com>
2023-02-10 20:12:26 -0700

    Update

--- client.cpp
+++ client.cpp
@@ -1,8 +1,19 @@
 #include <boost/asio.hpp>
-#include <boost/smart_ptr.hpp>
 #include <array>
 #include <iostream>
-#include "network.h"
+
+bool confirmWrite(boost::asio::ip::tcp::socket& socket, std::string cmd)
+{
+    boost::system::error_code error;   
+    boost::asio::write(socket, boost::asio::buffer(cmd), error);
+
+    std::array<char, 3> buf;
+    size_t len = boost::asio::read(socket, boost::asio::buffer(buf), error); //wait for reply
+    std::cout.write(buf.data(), len);
+    std::cout << std::endl;
+
+    return strcmp(buf.data(), "ok") == 0;
+}
 
 int main(int argc, char* argv[]) 
 {
@@ -26,7 +37,7 @@ int main(int argc, char* argv[])
         //system("read -p 'enter'");
         confirmWrite(socket, "ft /folder/file.txt"); //command with file dest
         //system("read -p 'enter'");
-        confirmWrite(socket, "512"); //file size
+        confirmWrite(socket, "14"); //file size
         //system("read -p 'enter'");
         boost::asio::write(socket, boost::asio::buffer("file contents"), error); //file
         //system("read -p 'enter'");
--- server.cpp
+++ server.cpp
@@ -1,12 +1,66 @@
 #include <boost/asio.hpp>
 #include <array>
 #include <iostream>
-#include "network.h"
 
-//! Server
-/*!
-Opens port for accepting.
-*/
+std::string unknownRead(boost::asio::ip::tcp::socket& socket) 
+{
+    boost::system::error_code error;
+
+    std::stringstream sstream;
+    for(;;) //loop read_some
+    {
+        if(!socket.available()) //check if all is read
+        {
+            break;
+        }
+
+        std::array<char, 128> buf;
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+        if(error == boost::asio::error::eof) 
+        {
+            break;
+        } 
+        else if(error) 
+        {
+            throw boost::system::system_error(error);
+        }
+
+        sstream.write(buf.data(), len);
+    }
+
+    boost::asio::write(socket, boost::asio::buffer("ok"), error); //response
+    return sstream.str();
+}
+
+bool cmdRead(boost::asio::ip::tcp::socket& socket) 
+{
+    boost::system::error_code error;
+    std::array<char, 3> buf;
+    size_t len = boost::asio::read(socket, boost::asio::buffer(buf), error);
+
+    if(!len) //check if disconnected
+    {
+        return false;
+    }
+
+    if(strncmp(buf.data(), "ft", 2) == 0) //check command
+    {
+        std::cout << unknownRead(socket) << std::endl; //read file dest then wait
+        socket.wait(socket.wait_read);
+
+        int fileSize = std::stoi(unknownRead(socket)); //read file size
+        std::cout << fileSize << std::endl;
+
+        std::vector<char> bufV(fileSize);
+        len = boost::asio::read(socket, boost::asio::buffer(bufV), error); //read file
+        std::cout.write(bufV.data(), len);
+        std::cout << std::endl;
+    }
+
+    return true;
+}
+
 int main() 
 {
     try 
@@ -18,13 +72,11 @@ int main()
         acceptor.accept(socket); //wait for connection
         std::cout << "start" << std::endl;
 
-        boost::system::error_code error;
-        std::array<char, 3> buf;
-        //system("read -p 'enter'");
-        cmdRead(socket); //wait for command
-        //system("read -p 'enter'");
-        //cmdRead(socket);
-
+        while(cmdRead(socket)) //wait for command unless disconnected
+        {
+            //system("read -p 'enter'");
+        }
+        
         std::cout << "end" << std::endl;
     } 
     catch(std::exception& e) 
